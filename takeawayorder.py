6c7c1b626dd54a31ebe3b3adfe5072b489b34ff9
Ting Yin Ho <tyh448@cs.bham.ac.uk>
2018-09-04 00:45:18 +0100

    First commit of order system

---
+++ takeawayorder.py
@@ -0,0 +1,26 @@
+
+print('Welcome to Eastern Express, please enter the number of the item you wish to order.\nOnce finished please type Finished to place the '
+      'order. May I take your order?')
+menu = {'1': 'Chicken Rice', '3': 'Chicken Balls'}
+price = {'Chicken Rice': 3.50, 'Chicken Balls': 2.60}
+total =[]
+orderedFood = []
+while True:
+    order = input("What is the number of your order? ")
+
+    if order == "Finished":
+        break
+
+    elif order not in menu:
+        print('Menu Item not found ')
+    else:
+        orderedFood.append(menu.get(order))
+        print("Your order current order is: " )
+        print(orderedFood)
+        total.append(price.get(menu.get(order)))
+
+totalPrice = sum(total)
+
+print(totalPrice)
+
+type(order)
