ce42ecb178f45d3833e7bd9034835ee4520e4128
Ting Yin Ho <tyh448@cs.bham.ac.uk>
2018-09-04 01:16:54 +0100

    Edited pricing

--- takeawayorder.py
+++ takeawayorder.py
@@ -2,7 +2,7 @@
 print('Welcome to Eastern Express, please enter the number of the item you wish to order.\nOnce finished please type Finished to place the '
       'order. May I take your order?')
 menu = {'1': 'Chicken Rice', '3': 'Chicken Balls'}
-price = {'Chicken Rice': 3.50, 'Chicken Balls': 2.60}
+price = {'Chicken Rice': 3.51, 'Chicken Balls': 2.60}
 total =[]
 orderedFood = []
 while True:
@@ -15,12 +15,15 @@ while True:
         print('Menu Item not found ')
     else:
         orderedFood.append(menu.get(order))
-        print("Your order current order is: " )
-        print(orderedFood)
+        print ("Your order current order is: " )
+        orderedFood.sort()
+        for e in orderedFood:
+            print(e + " £" + str(round(price.get(e),2)))
         total.append(price.get(menu.get(order)))
 
 totalPrice = sum(total)
 
-print("The total price of your order is £" + totalPrice)
+print("The total price of your order is £" + str(round(totalPrice,2)))
+
 
 type(order)
